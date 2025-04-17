be8764db719cc5362d3b61cd49bd1d2ae287903f
HSD06040 <tinto0604@gmail.com>
2025-04-17 19:19:35 +0900

    [Feat] SceneLoader
    Trigger또는 Distance Additive 방식으로 작동하는 씬로더 제작

---
+++ ProjectA/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    enum CheckType { Distance, Trigger }
+
+    [SerializeField] private Transform player;
+    [SerializeField] private CheckType checkType;
+    [SerializeField] private float distance;
+
+    private bool isLoaded;
+
+    private void Start()
+    {
+        if(SceneManager.sceneCount > 0)
+        {
+            for (int i = 0; i < SceneManager.sceneCount; i++)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+
+                if(scene.name == gameObject.name)
+                {
+                    isLoaded = true;
+                }
+            }
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (checkType == CheckType.Distance)
+            DistanceCheck();
+    }
+
+    private void DistanceCheck()
+    {
+        if(Vector3.Distance(player.position,transform.position) < distance)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnLoadScene();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            LoadScene();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            UnLoadScene();
+    }
+
+    private void LoadScene()
+    {
+        if(!isLoaded)
+        {
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            isLoaded = true;
+        }
+    }
+    private void UnLoadScene()
+    {
+        if(isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(gameObject.name);
+            isLoaded= false;
+        }
+    }
+}
